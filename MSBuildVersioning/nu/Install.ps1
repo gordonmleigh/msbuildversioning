fba60d2674c8451f251a14353ad0d6225f368eab
David Schmitt <david@dasz.at>
2012-10-09 12:42:11 +0200

    Update install.ps1 script to avoid re-installing if no reference was set

--- MSBuildVersioning/nu/Install.ps1
+++ MSBuildVersioning/nu/Install.ps1
@@ -61,54 +61,56 @@ $buildProject = $origProj | Get-MSBuildProject
 $buildProject.Items | Where-Object { $_.Key -eq 'Reference' } | Select-Object -ExpandProperty Value |  Select-Object -ExpandProperty EvaluatedInclude | Write-Host
 # remove reference
 $ref = $buildProject.Items | Where-Object { $_.Key -eq 'Reference' } | Select-Object -ExpandProperty Value | Where-Object { $_.EvaluatedInclude.StartsWith('MSBuildVersioning') }
-$buildProject.RemoveItem($ref)
+if ($ref) {
+    $buildProject.RemoveItem($ref)
 
-# figure out our installation path
-$csprojpath = [System.IO.PATH]::GetFullPath([System.IO.PATH]::GetDirectoryName($origProj.FullName))
-Write-Host "Using csprojpath=$csprojpath"
+    # figure out our installation path
+    $csprojpath = [System.IO.PATH]::GetFullPath([System.IO.PATH]::GetDirectoryName($origProj.FullName))
+    Write-Host "Using csprojpath=$csprojpath"
 
-$hintpath = $ref | Select-Object -ExpandProperty Metadata | Where-Object {$_.Name -eq 'HintPath' } | Select-Object -ExpandProperty EvaluatedValue
-Write-Host "Detected hintpath=$hintpath"
-if (-not [System.IO.PATH]::IsPathRooted($hintpath)) {
-    $hintpath = [System.IO.PATH]::Combine($csprojpath, $hintpath)
-}
-$hintpath = [System.IO.PATH]::GetDirectoryName([System.IO.PATH]::GetFullPath($hintpath))
-Write-Host "Using hintpath=$hintpath"
-
-$location = Get-RelativePath $csprojpath $hintpath
-Write-Host "Using location=$location"
-
-# add MSBuild targets
-$buildProject.Xml.AddImport("$location\MsBuildVersioning.targets")
-
-# avoid compiling the template
-$base = $buildProject.Items | Where-Object { $_.Key -eq 'Compile' } | Select-Object -ExpandProperty Value | Where-Object { $_.EvaluatedInclude -eq 'Properties\VersionInfo.cs' } 
-$base.ItemType = "None"
-
-# do compile the result
-$designer = $buildProject.Xml.AddItem("Compile", 'Properties\VersionInfo.Designer.cs')
-$designer.AddMetadata("DependentUpon", "VersionInfo.cs")
-
-# figure out which scm we're using and add a proper task
-$path = Get-Location
-$path = [System.IO.PATH]::GetFullPath($path)
-while ($path -ne [System.IO.PATH]::GetPathRoot($path)) {
-    Write-Host "Testing $path"
-    $type = @((Get-ChildItem $path -filter ".git" -force), (Get-ChildItem $path -filter ".hg" -force), (Get-ChildItem $path -filter ".svn" -force))
-    if ($type.Count -gt 0) {
-        $scm = $type[0].Name
-        Write-Host "Adding BeforeBuild Target for $path: $scm"
-        $target = $buildProject.Xml.AddTarget("BeforeBuild")
-        switch($scm) {
-            ".git" { $task = $target.AddTask("GitVersionFile") }
-            ".hg" { $task = $target.AddTask("HgVersionFile") }
-            ".svn" { $task = $target.AddTask("SvnVersionFile") }
-        }
-        $task.SetParameter("TemplateFile", 'Properties\VersionInfo.cs')
-        $task.SetParameter("DestinationFile", 'Properties\VersionInfo.Designer.cs')
-        break
+    $hintpath = $ref | Select-Object -ExpandProperty Metadata | Where-Object {$_.Name -eq 'HintPath' } | Select-Object -ExpandProperty EvaluatedValue
+    Write-Host "Detected hintpath=$hintpath"
+    if (-not [System.IO.PATH]::IsPathRooted($hintpath)) {
+        $hintpath = [System.IO.PATH]::Combine($csprojpath, $hintpath)
     }
-    $path = [System.IO.PATH]::GetFullPath([System.IO.PATH]::Combine($path, ".."))
+    $hintpath = [System.IO.PATH]::GetDirectoryName([System.IO.PATH]::GetFullPath($hintpath))
+    Write-Host "Using hintpath=$hintpath"
+
+    $location = Get-RelativePath $csprojpath $hintpath
+    Write-Host "Using location=$location"
+
+    # add MSBuild targets
+    $buildProject.Xml.AddImport("$location\MsBuildVersioning.targets")
+
+    # avoid compiling the template
+    $base = $buildProject.Items | Where-Object { $_.Key -eq 'Compile' } | Select-Object -ExpandProperty Value | Where-Object { $_.EvaluatedInclude -eq 'Properties\VersionInfo.cs' } 
+    $base.ItemType = "None"
+
+    # do compile the result
+    $designer = $buildProject.Xml.AddItem("Compile", 'Properties\VersionInfo.Designer.cs')
+    $designer.AddMetadata("DependentUpon", "VersionInfo.cs")
+
+    # figure out which scm we're using and add a proper task
+    $path = Get-Location
+    $path = [System.IO.PATH]::GetFullPath($path)
+    while ($path -ne [System.IO.PATH]::GetPathRoot($path)) {
+        Write-Host "Testing $path"
+        $type = @((Get-ChildItem $path -filter ".git" -force), (Get-ChildItem $path -filter ".hg" -force), (Get-ChildItem $path -filter ".svn" -force))
+        if ($type.Count -gt 0) {
+            $scm = $type[0].Name
+            Write-Host "Adding BeforeBuild Target for $path: $scm"
+            $target = $buildProject.Xml.AddTarget("BeforeBuild")
+            switch($scm) {
+                ".git" { $task = $target.AddTask("GitVersionFile") }
+                ".hg" { $task = $target.AddTask("HgVersionFile") }
+                ".svn" { $task = $target.AddTask("SvnVersionFile") }
+            }
+            $task.SetParameter("TemplateFile", 'Properties\VersionInfo.cs')
+            $task.SetParameter("DestinationFile", 'Properties\VersionInfo.Designer.cs')
+            break
+        }
+        $path = [System.IO.PATH]::GetFullPath([System.IO.PATH]::Combine($path, ".."))
+    }
 }
 
 $origProj.Save()
