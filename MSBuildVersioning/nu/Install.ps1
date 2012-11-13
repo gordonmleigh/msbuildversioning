d2145bd49f72f98fe9c29ee6131282ae0db2c190
David Schmitt <david@dasz.at>
2012-11-13 16:55:29 +0100

    Remove NuGetPowerTools dependency
    This dependency didn't work properly on upgrades. Therefore this commit pulls
    the required function from NuGetPowerTools into the install.ps1.

--- MSBuildVersioning/nu/Install.ps1
+++ MSBuildVersioning/nu/Install.ps1
@@ -1,5 +1,36 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
+# from NuGetPowerTools: https://github.com/davidfowl/NuGetPowerTools
+function Resolve-ProjectName {
+    param(
+        [parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ProjectName
+    )
+    
+    if($ProjectName) {
+        $projects = Get-Project $ProjectName
+    }
+    else {
+        # All projects by default
+        $projects = Get-Project
+    }
+    
+    $projects
+}
+
+function Get-MSBuildProject {
+    param(
+        [parameter(ValueFromPipelineByPropertyName = $true)]
+        [string[]]$ProjectName
+    )
+    Process {
+        (Resolve-ProjectName $ProjectName) | % {
+            $path = $_.FullName
+            @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($path))[0]
+        }
+    }
+}
+
 function Get-RelativePath {
 <#
 .SYNOPSIS
